4282fe6fa6e12b738736c3ff47f79236f0786fb8
suedarby <sue.a.darby@gmail.com>
2022-10-28 13:43:03 -0800

    add search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { StashComponent } from './stash/stash.component';
 import { StashFormComponent } from './stash/stash-form/stash-form.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'stash', component: StashComponent},
   { path: 'stashform', component: StashFormComponent},
+  { path: 'search', component: SearchComponent }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
+
 import { HomeComponent } from './home/home.component';
 import { StashComponent } from './stash/stash.component';
 
 
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { environment } from 'src/environments/environment';
+import { FormsModule } from '@angular/forms';
+
 import { DatabaseModule } from '@angular/fire/database';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
- declarations: [
+ declarations: [	
    AppComponent,
    HomeComponent,
    StashComponent,
- ],
+      SearchComponent
+   ],
  imports: [
     RouterModule,
    BrowserModule,
    DatabaseModule,
+
+   FormsModule,
+   NgbModule,
+   provideFirebaseApp(() => initializeApp(environment.firebase)),
+   provideAuth(() => getAuth()),
+   provideFirestore(() => getFirestore()),
+
  ],
  providers: [],
  bootstrap: [AppComponent],
