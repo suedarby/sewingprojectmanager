8421837bf10081e8afa6e4ecb5b6cf3f9d3e32fe
suedarby <sue.a.darby@gmail.com>
2022-11-07 08:41:28 -0900

    rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,84 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
-
-import { HomeComponent } from './home/home.component';
-import { StashComponent } from './stash/stash.component';
-
-
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-import { environment } from 'src/environments/environment';
 import { FormsModule } from '@angular/forms';
 
-import { DatabaseModule } from '@angular/fire/database';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { SearchComponent } from './search/search.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+
+//add additional imports below here
+
+
 
 @NgModule({
- declarations: [	
-   AppComponent,
-   HomeComponent,
-   StashComponent,
-      SearchComponent
-   ],
- imports: [
-    RouterModule,
-   BrowserModule,
-   DatabaseModule,
+  imports:      [ BrowserModule, FormsModule, RouterModule, AppRoutingModule,
+  //add additional imports below here
 
-   FormsModule,
-   NgbModule,
-   provideFirebaseApp(() => initializeApp(environment.firebase)),
-   provideAuth(() => getAuth()),
-   provideFirestore(() => getFirestore()),
+  ],
+  declarations: [ AppComponent, HomeComponent,
+  // add additional declarations below
 
- ],
- providers: [],
- bootstrap: [AppComponent],
- exports: [  AppComponent,
-  HomeComponent,
-  StashComponent,]
+  ],
+  bootstrap:    [ AppComponent ]
 })
+
+
+
+//start again
+
+// import { NgModule } from '@angular/core';
+// import { RouterModule } from '@angular/router';
+
+
+// import { HomeComponent } from './home/home.component';
+// import { StashComponent } from './stash/stash.component';
+
+
+// import { BrowserModule } from '@angular/platform-browser';
+// import { AppComponent } from './app.component';
+
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// import { environment } from 'src/environments/environment';
+// import { FormsModule } from '@angular/forms';
+
+// import { DatabaseModule } from '@angular/fire/database';
+// import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+// import { provideAuth,getAuth } from '@angular/fire/auth';
+// import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+// import { SearchComponent } from './search/search.component';
+
+
+
+
+// @NgModule({
+//  declarations: [
+//    AppComponent,
+//    HomeComponent,
+  //  StashComponent,
+  //     SearchComponent
+  //  ],
+//  imports: [
+//   imports:      [ BrowserModule, FormsModule ],
+
+
+  //   RouterModule,
+  //  BrowserModule,
+  //  DatabaseModule,
+
+  //  FormsModule,
+  //  NgbModule,
+  //  provideFirebaseApp(() => initializeApp(environment.firebase)),
+  //  provideAuth(() => getAuth()),
+  //  provideFirestore(() => getFirestore()),
+
+//  ],
+//  providers: [],
+//  bootstrap: [AppComponent],
+//  exports: [  AppComponent,
+//   HomeComponent,
+//   StashComponent,]
+// })
 export class AppModule { }
